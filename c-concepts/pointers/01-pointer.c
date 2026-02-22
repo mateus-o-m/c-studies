7b58d9de0fbeccbb0addc3bfe793a6dc5a76b555
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-02-22 12:29:20 -0300

    Adding visual description of program

--- c-concepts/pointers/01-pointer.c
+++ c-concepts/pointers/01-pointer.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+/*
+Program that show different ways that a pointer can be printed
+*/
 int main() {
 	int age;
-	int* p = &age;
+	int* agePtr = &age;
 
-	scanf ("%d", p);
-	printf ("%%p, p: %p\n", p);
-	printf ("%%d, *p: %d\n", *p);
-	printf ("%%p, &age: %p\n", &age);
-	printf ("%%d, age: %d\n", age);
+	printf ("POINTER USAGE/LOGIC\nType a integer value:");
+	scanf ("%d", agePtr);
+	printf ("%%p print format to agePtr: %p\n", agePtr);
+	printf ("%%d print format to *agePtr: %d\n", *agePtr);
+	printf ("%%p print format to &age: %p\n", &age);
+	printf ("%%d print format to age: %d\n", age);
+	printf ("\nagePtr has the same value of &age\n*agePtr has the same value of age");
 
 	return 0;
 }
