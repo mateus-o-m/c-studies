3955551a97514cdd3fa1b1c93fa8c728a5ae71b5
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-03-18 15:31:16 -0300

    Rework of pointer and pointerUse source codes

--- concepts/pointers/pointerUse.c
+++ concepts/pointers/pointerUse.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
+/*
+Programa que utiliza ponteiros para retornar multiplos valores em funções void
+*/
 void divide (int* num1, int* num2, int divisor) {
-	*num1 /= divisor;
-	*num2 /= divisor;
+   *num1 /= divisor;
+   *num2 /= divisor;
 }
 
 int main() {
-	int num1 = 150;
-	int num2 = 200;
-	int divisor = 2;
+   int num1, num2;
+   int divisor;
 
-	divide (&num1, &num2, divisor);
-	printf ("%d, %d", num1, num2);
+   printf ("MULTIPLE VALUES RETURNING FROM A FUNCTION\nType a integer value: ");
+   scanf ("%d", &num1);
+   printf ("Type another integer value: ");
+   scanf ("%d", &num2);
+   printf ("Type a integer divisor: ");
+   scanf ("%d", &divisor);
 
-	return 0;
+   printf ("Numbers before integer divison: %d, %d\n", num1, num2);
+   divide (&num1, &num2, divisor);
+   printf ("Numbers after integer division by %d: %d, %d", divisor, num1, num2);
+
+   return 0;
 }
